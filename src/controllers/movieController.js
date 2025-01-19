cf09c6571dd6cb2f3a1545373fd0685038224a48
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-01-19 16:55:35 +0200

    add movie controller and create page

---
+++ src/controllers/movieController.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const movieController = express.Router();
+
+movieController.get('/create' , (req,res) => {
+    res.render('create');
+})
+
+
+export default movieController;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import homeController from './controllers/homeController.js'
+import movieController from './controllers/movieController.js';
 
 const routes = express.Router();
 
 routes.use(homeController);
+routes.use(movieController);
 
 routes.get('*' , (req,res) => {
     res.render('404')
