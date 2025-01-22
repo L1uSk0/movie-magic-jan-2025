10993999a8ef7e6c6fdb597d8bc00b5de069058b
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-01-22 21:02:44 +0200

    add search by name

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -3,8 +3,14 @@ import { v4 as uuid} from 'uuid'
 
 
 export default {
-    getAll(){
-        return movies;
+    getAll(filter = {}){
+
+        let result = movies;
+
+        if (filter.search){
+            result = result.filter(movie => movie.title.toLocaleLowerCase().includes(filter.search.toLowerCase())); 
+        }
+        return result;
     },
 
     findOne(movieId){
