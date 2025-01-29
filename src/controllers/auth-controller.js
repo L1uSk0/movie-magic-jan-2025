e9b35c4065cbd0ead81c32d87123c4bf8649a934
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-01-29 18:34:37 +0200

    update nagivigation

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const authController = express.Router();
 
+authController.get('register' ,(req, res) => {
+  res.render('auth/register');
+})
+
 export default authController;
