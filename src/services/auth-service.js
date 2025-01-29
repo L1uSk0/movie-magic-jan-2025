23ce656b2be0f8c1818e9ab08c5a28f6ee140a60
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-01-29 19:25:29 +0200

    add loggin functionality with token jwd generation

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,9 +1,35 @@
 import User from "../models/User.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
-
+const SECRET = 'z1e34$hPyfiCHvKvfrnh8lt9b51uDC.5G7E.FzH/g87PSF5ZFUbsukt7aSG'
 
 export default {
     register (userData){
         return User.create(userData);
+    },
+    async login (email,password){
+        //check if user exists
+            const user = await User.findOne({email});
+            if(!user){
+                throw new Error('Invalid email or password');
+            }
+
+        //check password is correct
+            const isValid = await bcrypt.compare(password , user.password);
+            if(!isValid){
+                throw new Error('Invalid password');
+            }
+        //generate token
+        const payload = {
+            id: user.id,
+            email: user.email,
+        };
+
+        //TO DO : use async option
+            const token = jwt.sign(payload , SECRET, {expiresIn:'2h'});
+        //return token
+
+        return token;
     }
 }
