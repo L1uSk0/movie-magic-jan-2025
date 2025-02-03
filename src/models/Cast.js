641b8212915ad56d239724546b484b42ecd0ec8e
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-02-03 20:54:29 +0200

    add cast , user validations

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,10 +1,26 @@
 import {Schema,model} from "mongoose";
 
 const castSchema = new Schema({
-    name:String,
-    age:Number,
-    born:String,
-    imageUrl:String,
+    name:{
+        type:String,
+        required:[true,'Name is required'],
+        minLength:5,
+        match: [/^[A-Za-z 0-9]+$/ , 'Name should be alphanumeric , digits and whitespaces'],
+    },
+    age:{
+        type:Number,
+        min:0,
+        max:120,
+       },
+    born:{
+        type:String,
+        min:10,
+        match: /^[A-Za-z 0-9]+$/,
+       },
+    imageUrl:{
+        type:String,
+        match:/^https?:\/\//,
+    },
     
 });
 
--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,24 @@ import {Schema,model , Types} from "mongoose";
 import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
-    email:String,
-    password:String,
+    email:{
+        type:String,
+        match:/\@[a-zA-Z]+.[a-zA-Z]+$/,
+        minLength:10,
+    },
+    password:{
+        type:String,
+        match:/^[A-Za-z0-9]+$/,
+        minLength:6,
+    },
+});
+
+// 
+userSchema.virtual('rePassword')
+.set(function(rePassword){
+    if(rePassword !== this.password){
+        throw new Error('Password missmatch')
+    }
 });
 
 userSchema.pre('save' , async function(){
