7d93fd17beb94cd11f4e64b38bb047bdf024845c
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-02-03 20:01:52 +0200

    add Movie model validation

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -7,16 +7,44 @@ const movieSchema = new Schema({
     title:{
         type:String,
         required:[true,'Title is required'],
-        minlength:5,
-        
+        minLength:5,
+        maxLength:250,
+        match: [/^[A-Za-z 0-9]+$/ , 'Title should be alphanumeric , digits and whitespaces'],
     },
     category:String,
-    genre:String,
-    director:String,
-    year:Number,
-    imageUrl:String,
-    rating:Number,
-    description:String,
+    genre:{
+        type:String,
+        required:[true,'Genre is required'],
+        minLength:5,
+        maxLength:250,
+        match: [/^[A-Za-z 0-9]+$/ , 'Genre should be alphanumeric , digits and whitespaces'],
+    },
+    director:{
+        type:String,
+        required:[true,'Director is required'],
+        minLength:5,
+        maxLength:250,
+        match: [/^[A-Za-z 0-9]+$/ , 'Director should be alphanumeric , digits and whitespaces'],
+    },
+    year:{
+        type:Number,
+        min:1900,
+        max:2025,
+    },
+    imageUrl:{
+        type:String,
+        match:/^https?:\/\//,
+    },
+    rating:{
+        type:Number,
+        min:1,
+        max:10,
+    },
+    description:{
+        type:String,
+        minLength:20,
+        match:/^[A-Za-z 0-9]+$/,
+    },
     casts:[{
         type:Types.ObjectId,
         ref:'Cast',
